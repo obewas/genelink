b9bc3256343e60f936a5fabbac11c87e443a43c4
obewas <obewas1202@gmail.com>
2025-07-01 09:12:32 +0300

    created crud views

--- family/urls.py
+++ family/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.person_list, name='person_list'),
     path('add/', views.person_create, name='person_create'),
+    path('person/<int:pk>/', views.person_detail, name='person_detail'),
+    path('person/<int:pk>/edit/', views.person_update, name='person_update'),
+    path('person/<int:pk>/delete/', views.person_delete, name='person_delete'),
+    
 ]
+
+
 
--- family/views.py
+++ family/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Person
 from .forms import PersonForm
 
+
+# Create your views here.
 def person_list(request):
     people = Person.objects.all()
     return render(request, 'family/person_list.html', {'people': people})
+
+def person_detail(request, pk):
+    person = get_object_or_404(Person, pk=pk)
+    return render(request, 'family/person_detail.html', {'person': person})
 
 def person_create(request):
     if request.method == 'POST':
@@ -18,3 +21,21 @@ def person_create(request):
     else:
         form = PersonForm()
     return render(request, 'family/person_form.html', {'form': form})
+
+def person_update(request, pk):
+    person = get_object_or_404(Person, pk=pk)
+    if request.method == 'POST':
+        form = PersonForm(request.POST, instance=person)
+        if form.is_valid():
+            form.save()
+            return redirect('person_detail', pk=pk)
+    else:
+        form = PersonForm(instance=person)
+    return render(request, 'family/person_form.html', {'form': form})
+
+def person_delete(request, pk):
+    person = get_object_or_404(Person, pk=pk)
+    if request.method == 'POST':
+        person.delete()
+        return redirect('person_list')
+    return render(request, 'family/person_confirm_delete.html', {'person': person})
